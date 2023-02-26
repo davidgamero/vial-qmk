9a492d84cfbb8d559b1485e104d71a25a02bc577
David Gamero <david340804@gmail.com>
2023-02-26 13:33:41 -0500

    add wings

---
+++ keyboards/ymdk/wings/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID                                                      \
+  { 0x62, 0x87, 0x2F, 0x29, 0xC0, 0x4D, 0xF8, 0xE2 }
+#define VIAL_UNLOCK_COMBO_ROWS                                                 \
+  { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS                                                 \
+  { 0, 1 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define VIAL_TAP_DANCE_ENTRIES 10
+#define IGNORE_MOD_TAP_INTERRUPT
